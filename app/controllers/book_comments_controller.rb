f5784d5fc17aa1bd8ed8013f2660f16339ca2709
EC2 Default User <ec2-user@ip-172-31-13-68.ec2.internal>
2022-09-17 05:16:36 +0000

    [Update]コメントの非同期化

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,16 +1,17 @@
 class BookCommentsController < ApplicationController
   def create
-    book = Book.find(params[:book_id])
+    @book = Book.find(params[:book_id])
     @comment = current_user.book_comments.new(book_comment_params)
-    @comment.book_id = book.id
+    @comment.book_id = @book.id
     @comment.save
-    redirect_to book_path(book.id)
+    @book_comment = BookComment.new
+    #redirect_to book_path(book.id)
   end
 
   def destroy
     @comment = BookComment.find_by(id: params[:id], book_id: params[:book_id])
     @comment.destroy
-    redirect_to book_path(@comment.book.id)
+    #redirect_to book_path(@comment.book.id)
   end
 
   private
--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -13,12 +13,12 @@ class RelationshipsController < ApplicationController
   end
   
   def followings
-    user = User.find(params[:id])
+    user = User.find(params[:user_id])
 		@users = user.followings
   end
 
   def followers
-    user = User.find(params[:id])
+    user = User.find(params[:user_id])
 		@users = user.followers
   end
 end
